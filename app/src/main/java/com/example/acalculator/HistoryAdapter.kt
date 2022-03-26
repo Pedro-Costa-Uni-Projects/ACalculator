b53a95ad211fd8a466f438a1f679cdc75986dea5
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-03-26 21:42:32 +0000

    AULA 5 - EXERCICIOS TODOS FEITOS - COMMIT FINAL

--- app/src/main/java/com/example/acalculator/HistoryAdapter.kt
+++ app/src/main/java/com/example/acalculator/HistoryAdapter.kt
@@ -5,7 +5,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.acalculator.databinding.ItemExpressionBinding
 
-class HistoryAdapter(private val onOperationClick: (OperationUi) -> Unit,private val onOperationLongClick: (OperationUi) -> Unit,
+class HistoryAdapter(private val onOperationClick: (OperationUi) -> Unit,
+                     private val onOperationLongClick: (OperationUi) -> Unit,
                      private var items: List<OperationUi> = listOf()) : RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
 
     class HistoryViewHolder (val binding: ItemExpressionBinding) : RecyclerView.ViewHolder(binding.root)
