fd4eb97ecb0777a75e348e9a5201d11f03dc15eb
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-04-13 12:17:49 +0100

    AULA 7 - COMMIT FINAL -

--- app/src/main/java/com/example/acalculator/HistoryFragment.kt
+++ app/src/main/java/com/example/acalculator/HistoryFragment.kt
@@ -2,14 +2,11 @@ package com.example.acalculator
 
 import android.content.Context
 import android.content.pm.ActivityInfo
-import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.acalculator.databinding.FragmentHistoryBinding
